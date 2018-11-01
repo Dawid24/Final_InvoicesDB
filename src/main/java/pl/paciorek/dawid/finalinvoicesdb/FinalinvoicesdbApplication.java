0b946aeaabb9031516cba396dba7a897192bfb44
Dawid24 <paciorekdawidonly@gmail.com>
2018-11-01 13:31:28 +0100

    add a authentication

---
+++ src/main/java/pl/paciorek/dawid/finalinvoicesdb/FinalinvoicesdbApplication.java
@@ -0,0 +1,12 @@
+package pl.paciorek.dawid.finalinvoicesdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinalinvoicesdbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinalinvoicesdbApplication.class, args);
+	}
+}
